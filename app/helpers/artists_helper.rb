535060c988b3fede637474b49624e6bc85176e08
Hannah Malm <hannahemalm616@gmail.com>
2021-08-30 18:15:43 -0400

    commit tree e957eec3b2b07d1f9bea5858dad12e931c83fb23

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,14 @@
 module ArtistsHelper
+    def display_artist(song)
+        if song.artist && song.artist.name != ""
+            link_to song.artist.name, artist_path(song.artist)
+        else
+            link_to "Add Artist", edit_song_path(song)
+        end
+    end
 end
+
+#1. Write code for #artist_name and #artist_name= so an Artist can be retreived from and associated with a Song instance
+#2. Write a helper method(display_artist) in app/helpers file
+#If an artist is associted with song, return artist show page
+#If an artist is not associated with the song, return a link to songs edit page - link text Add Artist 
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,14 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    artist.name if artist
   end
 
   def artist_name=(name)
+    a = Artist.find_or_create_by(name: name)
+    self.artist = a
   end
 end
+
+#1. Write code for #artist_name and #artist_name= so an Artist can be retreived from and associated with a Song instance
+#this retreives a song and associates it with a song instance
